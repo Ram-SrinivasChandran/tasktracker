7b2ebb086c70b0d3c372978dff37b9d171c58f1b
Ram Srinivas C <ramsrinivas@breezeware.net>
2025-07-08 22:25:53 +0530

    added postman collection

---
+++ src/main/java/com/ram/tasktracker/Entity/Task.java
@@ -0,0 +1,28 @@
+package com.ram.tasktracker.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Entity
+@Slf4j
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "task", schema = "task_svc")
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "task_seq_gen")
+    @SequenceGenerator(name = "task_seq_gen", sequenceName = "task_seq", schema = "task_svc", allocationSize = 1)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "title", nullable = false)
+    private String title;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "status")
+    private String status;
+}
